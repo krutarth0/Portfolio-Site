5727c917d5a760ea224fe102a8ab759962306945
karan kataria <katariakaran22@gmail.com>
2020-09-17 14:36:18 +0530

    nav bar

--- psite/src/components/Buttons/SexyButton.js
+++ psite/src/components/Buttons/SexyButton.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import '../../static/Buttons/sexy_button.css'
 export default function SexyButton(props) {
+
+    var size = props.size && {
+      width:`${props.size[0]}vw`,
+      height:`${props.size[1]}vh`
+    }
+
+      var backgroundColor = props.transparent && {
+        backgroundColor:`${props.transparent ? "transparent" : null}`
+      }
+
+      var color = props.color && {
+        color : `${props.color ? props.color : null}`
+      }
+
     return (
       <div className="button-holder">
 
-        {props.size ? 
+       
           <a href={props.link &&`#/${props.link.split(" ").join("").split("/").join("")}`} 
            class="sexy-button"
            onClick={props.onClick}
            style={{
-            width:`${props.size[0]}vw`,
-            height:`${props.size[1]}vh`}}
+             ...size,...backgroundColor,...color     
+          }}
           >{props.name}</a>
-        : 
-        <a href={props.link &&`#/${props.link.split(" ").join("").split("/").join("")}`} 
-        class="sexy-button"
-        onClick={props.onClick}
-        >{props.name}</a>
-        }
+
       </div>
    
     )
